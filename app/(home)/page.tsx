033df0f665bb17caf3533bc2339cd72dd5b28da1
codeByNashrul <abidzar103010@gmail.com>
2025-06-13 22:10:36 +0700

    04. Create-Home-Carousel

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,8 @@
-export default async function Page() {
+import { HomeCarousel } from "@/components/shared/home/home-carousel";
+import data from "@/lib/data";
+
+export default async function page(){
   return (
-    <div>
-      <h1 className="h1-bold text-center p-10">Home Page Content</h1>
-    </div>
+    <HomeCarousel items={data.carousels}/>
   )
 }
--- lib/data.ts
+++ lib/data.ts
@@ -1,3 +1,5 @@
+import { url } from "inspector";
+
 const data = {
     headerMenus: [
         {
@@ -23,6 +25,29 @@ const data = {
         {
             name: "Customer Service",
             href: '/page/customer-service' 
+        },
+    ],
+    carousels: [
+        {
+            title: "Most Popular Book on Sale",
+            buttonCaption: "Shop Now",
+            image: "/images/5.jpg",
+            url: "/search?category=books",
+            isPublished: true,
+        },
+        {
+            title: "Best Seller Book on Sale",
+            buttonCaption: "Shop Now",
+            image: "/images/6.jpg",
+            url: "/search?category=books1",
+            isPublished: true,
+        },
+        {
+            title: "Best Deals Book on Sale",
+            buttonCaption: "Shop Now",
+            image: "/images/7.jpg",
+            url: "/search?category=books2",
+            isPublished: true,
         },
     ]
 }
